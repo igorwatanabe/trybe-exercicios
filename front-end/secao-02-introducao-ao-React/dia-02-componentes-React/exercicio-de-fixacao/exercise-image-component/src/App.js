03cfe3ddafc05355148a5cf513e700620b72e61f
Igor <eng.igorwatanabe@gmail.com>
2023-04-11 19:21:06 -0300

    exercicios dos dias 2.2, 3.1 e 3.2

---
+++ front-end/secao-02-introducao-ao-React/dia-02-componentes-React/exercicio-de-fixacao/exercise-image-component/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from './Image';
+import staringCat from './staringCat.jpg';
+
+function App() {
+  // const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
+  return (
+    <main>
+      <Image source={ staringCat } alternativeText= "Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
