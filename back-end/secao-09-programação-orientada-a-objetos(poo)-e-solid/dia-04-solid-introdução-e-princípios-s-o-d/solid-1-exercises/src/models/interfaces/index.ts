5825e0216ab42deca91c9304251a697edc0f53b5
Igor <eng.igorwatanabe@gmail.com>
2023-09-25 13:45:47 -0300

    dia 9.4 finalizado

---
+++ back-end/secao-09-programação-orientada-a-objetos(poo)-e-solid/dia-04-solid-introdução-e-princípios-s-o-d/solid-1-exercises/src/models/interfaces/index.ts
@@ -0,0 +1,7 @@
+export { INewPlant, IPlant } from './IPlant';
+
+export {
+    IModel, IModelDelete, IModelReader,
+    IModelWriter
+} from './IModel';
+export { default as IOpsInfo } from './IOpsInfo';
