8347a292d4fb7066722ffc7d1fa7973934fa9084
Igor <eng.igorwatanabe@gmail.com>
2023-10-17 21:31:44 -0300

    dia 9.5 finalizado

---
+++ back-end/secao-09-programação-orientada-a-objetos(poo)-e-solid/dia-05-solid-princípios-l-i/exercicios/veiculos/Car.ts
@@ -0,0 +1,17 @@
+// import { IVehicle } from './interfaces';
+
+// export default class Car implements IVehicle {
+//   drive(): void {
+//     console.log('Drive a car');
+//   }
+
+//   fly(): void {
+//     throw new Error('I don\'t fly');
+//   }
+// }
+
+import { ICar } from './interfaces';
+
+export default class Car implements ICar {
+  drive(): void { console.log('Drive a car'); }
+}
