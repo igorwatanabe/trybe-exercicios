e5d296334ca54231113e6c51b903fda9c44a5140
Igor <eng.igorwatanabe@gmail.com>
2023-10-19 16:19:28 -0300

    dia 1.1 de c.c.

---
+++ ciencia-da-computacao/secao-01-introdução-a-python/dia-01-aprendendo-python/exercicio02/ex04.py
@@ -0,0 +1,12 @@
+names = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]
+
+
+def find_biggest_name(names):
+    biggest_name = names[0]
+    for name in names:
+        if len(name) > len(biggest_name):
+            biggest_name = name
+    return biggest_name
+
+
+print(find_biggest_name(names))
